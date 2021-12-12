c0835cc8857fabce9d797781392249a0c7eeb02b
OscarSah <oscar@cybertekschool.com>
2021-12-12 19:22:53 +0300

    Second Week Class Review Second Task started

---
+++ src/test/java/com/cybertek/tests/reviews/week02/Task2.java
@@ -0,0 +1,11 @@
+package com.cybertek.tests.reviews.week02;
+
+public class Task2 {
+    /*
+    go to https://www.demoblaze.com/index.html#
+    Navigate to Laptops Category
+    Then Sony vaio i7 laptop
+    Verify the product price is 790 Dollars
+     */
+
+}
