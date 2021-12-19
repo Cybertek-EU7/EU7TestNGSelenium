56e7c6ab6b731135cb80a75b980b1918e9a40bb2
OscarSah <oscar@cybertekschool.com>
2021-12-19 18:48:41 +0300

    Homework Started

---
+++ src/test/java/com/cybertek/tests/reviews/week03/CheckBoxHW.java
@@ -0,0 +1,92 @@
+package com.cybertek.tests.reviews.week03;
+
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+public class CheckBoxHW {
+/*
+Test Case Verify CheckBox CheckAll and UncheckAll Buttons
+1.    Go to http://secure.smartbearsoftware.com/samples/TestComplete12/WebOrders/Login.aspx
+2.    Login with-----Username: Tester, password: test
+       // Test Case 1
+3.    Click on check all button verify all the checkboxes are checked
+4.    Click on uncheck all button verify that all the checkboxes are unchecked
+       // Test Case 2
+5.    Select one of the checkbox and delete one person
+6.    Then verify that deleted item is no longer exists
+
+    What is the precondition for both Test Cases? Login to webPage
+    Can I out the same precondition to BeforeMethod? Yes
+ */
+    public WebDriver driver; // declare your variable/object globally and use it at every method
+    @BeforeClass             // annotations are there to effect your test flow
+    public void setClass(){
+        driver = WebDriverFactory.getDriver("chrome"); // initialize our driver object
+    }
+
+    @BeforeMethod
+    public void openPage(){
+        driver.get("http://secure.smartbearsoftware.com/samples/TestComplete12/WebOrders/Login.aspx");
+        driver.manage().window().maximize();
+        // Login to WebPage
+        WebElement userNameBox = driver.findElement(By.id("ctl00_MainContent_username"));
+        userNameBox.sendKeys("Tester");
+        WebElement passwordBox = driver.findElement(By.cssSelector("input[name='ctl00$MainContent$password']"));
+        passwordBox.sendKeys("test");
+        WebElement submitButton = driver.findElement(By.cssSelector("input[type='submit']"));
+        submitButton.click();
+    }
+
+    @AfterMethod
+    public void closeBrowser(){
+        driver.close();
+    }
+
+    @AfterClass
+    public void killSession(){
+        driver.quit();
+    }
+
+
+    @Test
+    public void checkBoxAll(){
+        // Click on check all button
+            WebElement checkAllButton = driver.findElement(By.linkText("Check All"));                   checkAllButton.click();
+        // verify all the checkboxes are checked
+
+    // (//input[@type='checkbox'])[1] for the first
+        // (//input[@type='checkbox'])[2]
+// findElements method : can get actually dynamically row number
+       /*
+      Assert.assertTrue(driver.findElement(By.xpath("(//input[@type='checkbox'])[1]")).isSelected()); //1
+      Assert.assertTrue(driver.findElement(By.xpath("(//input[@type='checkbox'])[2]")).isSelected()); //2
+      Assert.assertTrue(driver.findElement(By.xpath("(//input[@type='checkbox'])[3]")).isSelected()); //3
+      Assert.assertTrue(driver.findElement(By.xpath("(//input[@type='checkbox'])[4]")).isSelected()); //4
+      Assert.assertTrue(driver.findElement(By.xpath("(//input[@type='checkbox'])[5]")).isSelected()); //5
+        */
+
+
+        for (int i = 1; i <= 8 ; i++) {
+            String rowChechBoxLocator = "(//input[@type='checkbox'])["+i+"]"; // String Concatination
+            WebElement rowCheckBox = driver.findElement(By.xpath(rowChechBoxLocator));
+            Assert.assertTrue(rowCheckBox.isSelected(),"The CheckBox IS NOT selected");
+        }
+
+
+
+    }
+
+    @Test
+    public void deletePersonTest(){
+
+    }
+
+
+
+
+
+}
