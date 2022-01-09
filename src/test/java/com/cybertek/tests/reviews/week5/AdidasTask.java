e284261fa67d2d3816b0f8348a43f6512402f641
OscarSah <oscar@cybertekschool.com>
2022-01-09 19:25:53 +0300

    Adidas Task method created

--- src/test/java/com/cybertek/tests/reviews/week5/AdidasTask.java
+++ src/test/java/com/cybertek/tests/reviews/week5/AdidasTask.java
@@ -1,8 +1,11 @@
 package com.cybertek.tests.reviews.week5;
 
 import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -39,16 +42,25 @@ You have to implement the following Web automated checks over our DEMO ONLINE SH
     }
 
     public void navigateTo(String str){
-        driver.findElement(By.linkText(str)).click();
+        driver.findElement(By.partialLinkText(str)).click();
+    }
+
+    public void productAdder(String category, String product){
+        navigateTo(category);
+        navigateTo(product);
+        navigateTo("Add to cart");
+        // handle pop up
+        WebDriverWait wait = new WebDriverWait(driver,10);
+        wait.until(ExpectedConditions.alertIsPresent());
+        Alert alert = driver.switchTo().alert();
+        alert.accept();
+        navigateTo("Home");
     }
 
     @Test
     public void Test(){
         // Navigate to "Laptop" → "Sony vaio i5" and click on "Add to cart". Accept pop up confirmation.
-        navigateTo("Laptops");
-        navigateTo("Sony vaio i5");
-        navigateTo("Add to cart");
-
+        productAdder("Laptop","Sony vaio i5");
     }
 
 }
