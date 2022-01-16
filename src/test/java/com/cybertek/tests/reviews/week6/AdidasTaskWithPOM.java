734e360e79b5eaca52b4e3fc4ba57cd6e478ba6f
OscarSah <oscar@cybertekschool.com>
2022-01-16 22:01:44 +0300

    Week 6 creating files from scratch

---
+++ src/test/java/com/cybertek/tests/reviews/week6/AdidasTaskWithPOM.java
@@ -0,0 +1,14 @@
+package com.cybertek.tests.reviews.week6;
+
+import com.cybertek.tests.TestBaseTwo;
+import org.testng.annotations.Test;
+
+public class AdidasTaskWithPOM extends TestBaseTwo {
+
+    @Test
+    public void PurchaseTest(){
+
+    }
+
+
+}
