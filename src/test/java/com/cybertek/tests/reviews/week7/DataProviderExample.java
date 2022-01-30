2aabf45abf5807b8a187ae9f85d055eb7a18a288
OscarSah <oscar@cybertekschool.com>
2022-01-30 22:21:39 +0300

    Data Provider Finished

--- src/test/java/com/cybertek/tests/reviews/week7/DataProviderExample.java
+++ src/test/java/com/cybertek/tests/reviews/week7/DataProviderExample.java
@@ -5,10 +5,7 @@ import org.apache.poi.hssf.record.chart.DatRecord;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 import java.util.concurrent.TimeUnit;
 
@@ -16,9 +13,9 @@ public class DataProviderExample {
     /*
     Step 1. Go to “https://practice-cybertekschool.herokuapp.com”
     Step 2. And click on “Status Codes”.
-    Step 3. Then click on “200”.
+    Step 3. Then click on “301”.
     Step 4. Verify that following message is displayed:
-    “This page returned a 200 status code”
+    “This page returned a 301 status code”
      */
     WebDriver driver;
     @BeforeMethod
@@ -34,11 +31,37 @@ public class DataProviderExample {
     public void tearDown(){
         Driver.closeDriver();
     }
+    @DataProvider
+    public Object [][] getData(){
+        String [][] data = new String[4][2];
+        // test case number 9
+        data[0][0] = "200";
+        data[0][1] = "This page returned a 200 status code";
+        // test case number 10
+        data[1][0] = "301";
+        data[1][1] = "This page returned a 301 status code";
+    // test case number 11
+        data[2][0] = "404";
+        data[2][1] = "This page returned a 404 status code";
+        // test case number 12
+        data[3][0] = "500";
+        data[3][1] = "This page returned a 500 status code";
 
-    @Test
-    public void StatusCodesTests(){
-        String code = "200";
-        String expectedMessage = "This page returned a 200 status code";
+        /*
+        String[][] data = {
+                {"200","This page returned a 200 status code."},
+                {"301","This page returned a 301 status code."},
+                {"404","This page returned a 404 status code."},
+                {"500","This page returned a 500 status code."}
+        };
+         */
+        return data;
+    }
+
+    @Test(dataProvider = "getData")
+    public void StatusCodesTests(String code, String expectedMessage){
+       // String code = "301";
+       //  String expectedMessage = "This page returned a 301 status code";
         driver.findElement(By.linkText(code)).click();  // clicks on 200 status code
 
         String actualMessage = driver.findElement(By.tagName("p")).getText();
